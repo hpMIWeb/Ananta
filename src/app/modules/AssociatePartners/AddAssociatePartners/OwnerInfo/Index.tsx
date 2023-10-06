f660c7e426842e1db5a73053b1c52f5124fd7a4d
Pinank Soni <pinank@miwebsolution.com>
2023-10-06 10:35:46 +0530

    Associate partner changes

--- src/app/modules/AssociatePartners/AddAssociatePartners/OwnerInfo/Index.tsx
+++ src/app/modules/AssociatePartners/AddAssociatePartners/OwnerInfo/Index.tsx
@@ -12,12 +12,12 @@ const OwnerInfo = ({ onChange, setFormValue, clientType }: any) => {
         { type: "default", index: 0, name: "index0" },
     ]);
     const onFinish = (value: any) => {
-        const filteredValue = filterObjectByKey(
-            value.ownerDetails,
-            ownerInfoData.map((a) => a.name)
-        );
+        // const filteredValue = filterObjectByKey(
+        //     value.ownerDetails,
+        //     ownerInfoData.map((a) => a.name)
+        // );
 
-        setFormValue({ ownerDetails: Object.values(filteredValue) });
+        // setFormValue({ ownerDetails: Object.values(filteredValue) });
         onChange(4);
     };
 
@@ -52,31 +52,6 @@ const OwnerInfo = ({ onChange, setFormValue, clientType }: any) => {
                 requiredMark={false}
                 className="customAddForm"
             >
-                <Form.List name="ownerDetails">
-                    {(fields, { add, remove }) => (
-                        <>
-                            {ownerInfoData.map((field, index) => (
-                                <div
-                                    style={{ marginTop: 2 }}
-                                    key={index}
-                                    className="row"
-                                >
-                                    {/* {form.type === "new" && (
-                    <hr className={styles.ownerInfoCardLine} />
-                  )} */}
-                                    <OwnerInfoCardBox
-                                        index={index}
-                                        field={field}
-                                        remove={remove}
-                                        //  canDelete={form.type === "new"}
-                                        onDeleteCardClick={onDeleteCardClick}
-                                        clientType={clientType}
-                                    />
-                                </div>
-                            ))}
-                        </>
-                    )}
-                </Form.List>
                 <div className="row">
                     <div className={styles.formFooterAction}>
                         <CardBottomAction
