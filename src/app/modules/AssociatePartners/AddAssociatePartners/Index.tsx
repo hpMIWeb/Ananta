8f337b25c005298a93bc7e388a81db4a0f30bc92
Pinank Soni <pinank@miwebsolution.com>
2023-10-20 15:17:54 +0530

    associate partner changes

--- src/app/modules/AssociatePartners/AddAssociatePartners/Index.tsx
+++ src/app/modules/AssociatePartners/AddAssociatePartners/Index.tsx
@@ -18,303 +18,277 @@ import Select from "../../../../components/Select/Index";
 import RevenueProgram from "./RevenueProgram/Index";
 import SubscriptionTabAddClient from "./SubscriptionTabAddClient/Index";
 import PaymentTabAddClient from "./PaymentTabAddClient/Index";
+import Cookies from "js-cookie";
 
 const AddAssociatePartners = () => {
-    const [activeTab, setActiveTab] = useState(1);
-    const [associatePartnerValue, setAssociatePartner] = useState({});
-    const dispatch = useAppDispatch();
-    const navigation = useNavigate();
-    const { clientId } = useParams();
-    const [form] = Form.useForm();
-    const getClientsListSuccess = useSelector(
-        (state: any) => state.getClients.success
-    );
-    const getClientsListLoading = useSelector(
-        (state: any) => state.getClients.loading
-    );
-    const [partnerType, setPartnerType] = useState("ca");
-    const [partnerTypeLabel, setPartnerTypeLabel] = useState("Partners ");
-    const [partnerCategory, setPartnerCategory] = useState("");
+  const [activeTab, setActiveTab] = useState(1);
+  const [associatePartnerValue, setAssociatePartner] = useState({});
+  const dispatch = useAppDispatch();
+  const navigation = useNavigate();
+  const { clientId } = useParams();
+  const [form] = Form.useForm();
+  const roleType = Cookies.get("roleTypeName");
+  const getClientsListSuccess = useSelector(
+    (state: any) => state.getClients.success
+  );
+  const getClientsListLoading = useSelector(
+    (state: any) => state.getClients.loading
+  );
+  const [partnerType, setPartnerType] = useState<string>("");
+  const [partnerTypeLabel, setPartnerTypeLabel] = useState("Partner ");
+  const [partnerCategory, setPartnerCategory] = useState("");
 
-    const getClientsList = useSelector((state: any) => state.getClients.data);
-    const { loading, success } = useSelector(
-        (state: any) => state.createClient
-    );
-    const partnerTypeOption = [
-        { value: "ca", label: "CA" },
-        {
-            value: "accountant",
-            label: "Accountant",
-        },
-        {
-            value: "tax_consultant",
-            label: "Tax Consultant",
-        },
-        {
-            value: "business_enterprise",
-            label: "Business Enterprise",
-        },
-        {
-            value: "other",
-            label: "Other",
-        },
-    ];
+  const getClientsList = useSelector((state: any) => state.getClients.data);
+  const { loading, success } = useSelector((state: any) => state.createClient);
+  const partnerTypeOption = [
+    { value: "ca", label: "CA" },
+    {
+      value: "accountant",
+      label: "Accountant",
+    },
+    {
+      value: "tax_consultant",
+      label: "Tax Consultant",
+    },
+    {
+      value: "business_enterprise",
+      label: "Business Enterprise",
+    },
+    {
+      value: "other",
+      label: "Other",
+    },
+  ];
 
-    useEffect(() => {
-        //TODO:: Need to convert in GET API
-        if (!getClientsListSuccess) {
-            dispatch(getClientsReducersApi());
-        }
-        dispatch(getSubscriptionsListApi());
-    }, []);
+  useEffect(() => {
+    //TODO:: Need to convert in GET API
+    if (!getClientsListSuccess) {
+      dispatch(getClientsReducersApi());
+    }
+    dispatch(getSubscriptionsListApi());
+  }, []);
 
-    const setFormValue = (formValue: any) => {
-        setAssociatePartner((prev) => ({ ...prev, ...formValue }));
-    };
+  const setFormValue = (formValue: any) => {
+    setAssociatePartner((prev) => ({ ...prev, ...formValue }));
+  };
 
-    const handleCancelClick = () => {
-        navigation("/associatePartners");
-    };
+  const handleCancelClick = () => {
+    navigation("/associatePartners");
+  };
 
-    useEffect(() => {
-        if (getClientsList.length && clientId) {
-            const currentCardDetail = getClientsList.find(
-                (s: any) => s._id === clientId
-            );
-            form.setFieldsValue(currentCardDetail);
-        }
-    }, [getClientsList, clientId, form]);
+  useEffect(() => {
+    if (getClientsList.length && clientId) {
+      const currentCardDetail = getClientsList.find(
+        (s: any) => s._id === clientId
+      );
+      form.setFieldsValue(currentCardDetail);
+    }
+  }, [getClientsList, clientId, form]);
 
-    useEffect(() => {
-        if (success) {
-            navigation("/associatePartners");
-        }
-    }, [success]);
+  useEffect(() => {
+    if (success) {
+      navigation("/associatePartners");
+    }
+  }, [success]);
 
-    const onChange = (key: number, formValue: any) => {
-        if (key === 5) {
-            const payload = { ...associatePartnerValue, ...formValue };
+  const onChange = (key: number, formValue: any) => {
+    if (key === 5) {
+      const payload = { ...associatePartnerValue, ...formValue };
 
-            payload.partnerType = partnerType;
-            payload.industryType = "Gold";
-            payload.lineOfBusiness = "Gold";
-            payload.fileNumber = "cnjkdcjk";
-            // @ts-ignore
-            dispatch(createAssociatePartnerReducersApi({ payload: payload }));
-        } else {
-            setActiveTab(key);
-        }
-    };
+      payload.partnerType = partnerType;
+      // @ts-ignore
+      dispatch(createAssociatePartnerReducersApi({ payload: payload }));
+    } else {
+      setActiveTab(key);
+    }
+  };
 
-    const items = [
-        {
-            key: 1,
-            label: `Firm Info`,
-            children: (
-                <BasicInfo
-                    onChange={onChange}
-                    setFormValue={setFormValue}
-                    partnerType={partnerType}
-                />
-            ),
-        },
-        {
-            key: 2,
-            label: `Owner Details`,
-            children: (
-                <OwnerInfo
-                    onChange={onChange}
-                    setFormValue={setFormValue}
-                    partnerType={partnerType}
-                />
-            ),
-        },
-        {
-            key: 3,
-            label:
-                partnerTypeLabel === "Sales Partner"
-                    ? `Revenue Program`
-                    : `Billing`,
-            children:
-                partnerTypeLabel === "Sales Partner" ? (
-                    <RevenueProgram
-                        onChange={onChange}
-                        setFormValue={setFormValue}
-                        partnerType={partnerType}
-                    />
-                ) : (
-                    <SubscriptionTabAddClient
-                        onChange={onChange}
-                        setFormValue={setFormValue}
-                    />
-                ),
-        },
-        {
-            key: 4,
-            label:
-                partnerTypeLabel === "Sales Partner"
-                    ? `Bank Details`
-                    : `Payment`,
-            children:
-                partnerTypeLabel === "Sales Partner" ? (
-                    <BankDetails
-                        onChange={onChange}
-                        setFormValue={setFormValue}
-                        partnerType={partnerType}
-                    />
-                ) : (
-                    <PaymentTabAddClient
-                        onChange={onChange}
-                        setFormValue={setFormValue}
-                    />
-                ),
-        },
-    ];
+  const items = [
+    {
+      key: 1,
+      label: `Firm Info`,
+      children: (
+        <BasicInfo
+          onChange={onChange}
+          setFormValue={setFormValue}
+          partnerType={partnerType}
+        />
+      ),
+    },
+    {
+      key: 2,
+      label: `Owner Details`,
+      children: (
+        <OwnerInfo
+          onChange={onChange}
+          setFormValue={setFormValue}
+          partnerType={partnerType}
+        />
+      ),
+    },
+    {
+      key: 3,
+      label:
+        partnerTypeLabel !== "Service Partner" ? `Revenue Program` : `Billing`,
+      children:
+        partnerTypeLabel !== "Service Partner" ? (
+          <RevenueProgram
+            onChange={onChange}
+            setFormValue={setFormValue}
+            partnerType={partnerType}
+          />
+        ) : (
+          <SubscriptionTabAddClient
+            onChange={onChange}
+            setFormValue={setFormValue}
+          />
+        ),
+    },
+    {
+      key: 4,
+      label:
+        partnerTypeLabel !== "Service Partner" ? `Bank Details` : `Payment`,
+      children:
+        partnerTypeLabel !== "Service Partner" ? (
+          <BankDetails
+            onChange={onChange}
+            setFormValue={setFormValue}
+            partnerType={partnerType}
+          />
+        ) : (
+          <PaymentTabAddClient
+            onChange={onChange}
+            setFormValue={setFormValue}
+          />
+        ),
+    },
+  ];
 
-    return (
+  return (
+    <div className={classNames("card mb-3", styles.addPromoCodeCardWrapper)}>
+      <div
+        className={classNames(
+          "card-header d-flex",
+          styles.promoCodeCardHeaderBox
+        )}
+        style={{ minHeight: 60 }}
+      >
         <div
-            className={classNames("card mb-3", styles.addPromoCodeCardWrapper)}
+          className={classNames(
+            "d-flex align-items-center w-100",
+            styles.promocodeHeaderTitle
+          )}
         >
-            <div
-                className={classNames(
-                    "card-header d-flex",
-                    styles.promoCodeCardHeaderBox
-                )}
-                style={{ minHeight: 60 }}
+          <div className="me-auto">
+            <h5
+              className={classNames(
+                "my-2 position-relative z-index-1",
+                styles.addPromoCodeLabel
+              )}
             >
-                <div
-                    className={classNames(
-                        "d-flex align-items-center w-100",
-                        styles.promocodeHeaderTitle
-                    )}
-                >
-                    <div className="me-auto">
-                        <h5
-                            className={classNames(
-                                "my-2 position-relative z-index-1",
-                                styles.addPromoCodeLabel
-                            )}
-                        >
-                            Add New Associate Partner
-                        </h5>
-                    </div>
-                    <div className={classNames("ms-auto z-index-1")}>
-                        <Button
-                            onClick={handleCancelClick}
-                            style={{
-                                minWidth: 104,
-                            }}
-                            className="greyBtn"
-                        >
-                            Cancel
-                        </Button>
-                    </div>
-                </div>
-            </div>
-            <div className={styles.addClientDetailBox}>
-                {getClientsListLoading && clientId && (
-                    <FormContentSkeletonLoader />
-                )}
-                <div className="row">
-                    <div className={classNames("col-12 col-md-4 col-lg-4")}>
-                        <div className="mb-3">
-                            <label
-                                className={classNames(
-                                    "form-label",
-                                    styles.rowTitle
-                                )}
-                            >
-                                Partners Category
-                                <sup className="text-danger fs--1">*</sup>
-                            </label>
-                            <Form.Item
-                                name="partnerCategory"
-                                className="customAddClientSelectOptions"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: "Please Select Category!",
-                                    },
-                                ]}
-                            >
-                                <Select
-                                    options={[
-                                        {
-                                            value: "sales_partner",
-                                            label: "Sales Partner",
-                                        },
-                                        {
-                                            value: "service_partner",
-                                            label: "Service Partner",
-                                        },
-                                    ]}
-                                    placeholder="Select Type"
-                                    onChange={(value: any) => {
-                                        setPartnerCategory(value);
-                                        if (value === "sales_partner") {
-                                            setPartnerTypeLabel(
-                                                "Sales Partner"
-                                            );
-                                        } else {
-                                            setPartnerTypeLabel(
-                                                "Service Partner"
-                                            );
-                                        }
-                                    }}
-                                />
-                            </Form.Item>
-                        </div>
-                    </div>
-                    <div className={classNames("col-12 col-md-4 col-lg-4")}>
-                        <div className="mb-3">
-                            <label
-                                className={classNames(
-                                    "form-label",
-                                    styles.rowTitle
-                                )}
-                            >
-                                {partnerTypeLabel} Type
-                                <sup className="text-danger fs--1">*</sup>
-                            </label>
-                            <Form.Item
-                                name="partnerType"
-                                className="customAddClientSelectOptions"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: "Please Enter Client Type!",
-                                    },
-                                ]}
-                            >
-                                <Select
-                                    options={partnerTypeOption.map(
-                                        (partnerType: any) => ({
-                                            label: partnerType?.label,
-                                            value: partnerType?._id,
-                                        })
-                                    )}
-                                    placeholder="Select Type"
-                                    onChange={(value: any) =>
-                                        setPartnerType(value)
-                                    }
-                                />
-                            </Form.Item>
-                        </div>
-                    </div>
-                </div>
-                {!(getClientsListLoading && clientId) && (
-                    <Tabs
-                        // tabBarExtraContent={operations}
-                        className="subscriptionTabs"
-                        defaultActiveKey="1"
-                        activeKey={activeTab}
-                        items={items}
-                        onChange={onChange}
-                    />
-                )}
-            </div>
+              Add New Associate Partner
+            </h5>
+          </div>
+          <div className={classNames("ms-auto z-index-1")}>
+            <Button
+              onClick={handleCancelClick}
+              style={{
+                minWidth: 104,
+              }}
+              className="greyBtn"
+            >
+              Cancel
+            </Button>
+          </div>
         </div>
-    );
+      </div>
+      <div className={styles.addClientDetailBox}>
+        {getClientsListLoading && clientId && <FormContentSkeletonLoader />}
+        <div className="row">
+          {roleType === "client" && (
+            <div className={classNames("col-12 col-md-4 col-lg-4")}>
+              <div className="mb-3">
+                <label className={classNames("form-label", styles.rowTitle)}>
+                  Partners Category
+                  <sup className="text-danger fs--1">*</sup>
+                </label>
+                <Form.Item
+                  name="partnerCategory"
+                  className="customAddClientSelectOptions"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please Select Category!",
+                    },
+                  ]}
+                >
+                  <Select
+                    options={[
+                      {
+                        value: "sales_partner",
+                        label: "Sales Partner",
+                      },
+                      {
+                        value: "service_partner",
+                        label: "Service Partner",
+                      },
+                    ]}
+                    placeholder="Select Type"
+                    onChange={(value: any) => {
+                      setPartnerCategory(value);
+                      if (value === "sales_partner") {
+                        setPartnerTypeLabel("Sales Partner");
+                      } else {
+                        setPartnerTypeLabel("Service Partner");
+                      }
+                    }}
+                  />
+                </Form.Item>
+              </div>
+            </div>
+          )}
+
+          <div className={classNames("col-12 col-md-4 col-lg-4")}>
+            <div className="mb-3">
+              <label className={classNames("form-label", styles.rowTitle)}>
+                {partnerTypeLabel} Type
+                <sup className="text-danger fs--1">*</sup>
+              </label>
+              <Form.Item
+                name="partnerType"
+                className="customAddClientSelectOptions"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please Enter Client Type!",
+                  },
+                ]}
+              >
+                <Select
+                  options={partnerTypeOption.map((partnerType: any) => ({
+                    label: partnerType?.label,
+                    value: partnerType?.value,
+                  }))}
+                  placeholder="Select Type"
+                  onChange={(value: any) => setPartnerType(value)}
+                />
+              </Form.Item>
+            </div>
+          </div>
+        </div>
+        {partnerType && partnerType !== "" && (
+          <Tabs
+            // tabBarExtraContent={operations}
+            className="subscriptionTabs"
+            defaultActiveKey="1"
+            activeKey={activeTab}
+            items={items}
+            onChange={onChange}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AddAssociatePartners;
--- src/app/modules/AssociatePartners/AddAssociatePartners/OwnerInfo/Index.tsx
+++ src/app/modules/AssociatePartners/AddAssociatePartners/OwnerInfo/Index.tsx
@@ -6,88 +6,82 @@ import { useState } from "react";
 import CardBottomAction from "./CardBottomAction";
 import { filterObjectByKey } from "../../../../../utils/helpers";
 
-const OwnerInfo = ({ onChange, setFormValue, clientType }: any) => {
-    const [form] = Form.useForm();
-    const [ownerInfoData, setOwnerInfoData] = useState([
-        { type: "default", index: 0, name: "index0" },
+const OwnerInfo = ({ onChange, setFormValue, partnerType }: any) => {
+  const [form] = Form.useForm();
+  const [ownerInfoData, setOwnerInfoData] = useState([
+    { type: "default", index: 0, name: "index0" },
+  ]);
+  const onFinish = (value: any) => {
+    const filteredValue = filterObjectByKey(
+      value.ownerDetails,
+      ownerInfoData.map((a) => a.name)
+    );
+
+    setFormValue({ ownerDetails: Object.values(filteredValue) });
+    onChange(4);
+  };
+
+  const ownerDetailsFormValue = Form.useWatch("ownerDetails", form) || {};
+
+  const addMoreOwnerCard = () => {
+    setOwnerInfoData((prev) => [
+      ...prev,
+      {
+        type: "new",
+        index: ownerInfoData.length,
+        name: `index${ownerInfoData.length}`,
+      },
     ]);
-    const onFinish = (value: any) => {
-        const filteredValue = filterObjectByKey(
-            value.ownerDetails,
-            ownerInfoData.map((a) => a.name)
-        );
+  };
 
-        setFormValue({ ownerDetails: Object.values(filteredValue) });
-        onChange(4);
-    };
+  const onDeleteCardClick = (cardIndex: any) => {
+    const newOwnerInfoData = ownerInfoData.filter((a) => a.index !== cardIndex);
+    setOwnerInfoData(newOwnerInfoData);
+  };
 
-    const ownerDetailsFormValue = Form.useWatch("ownerDetails", form) || {};
-
-    const addMoreOwnerCard = () => {
-        setOwnerInfoData((prev) => [
-            ...prev,
-            {
-                type: "new",
-                index: ownerInfoData.length,
-                name: `index${ownerInfoData.length}`,
-            },
-        ]);
-    };
-
-    const onDeleteCardClick = (cardIndex: any) => {
-        const newOwnerInfoData = ownerInfoData.filter(
-            (a) => a.index !== cardIndex
-        );
-        setOwnerInfoData(newOwnerInfoData);
-    };
-
-    return (
-        <div>
-            <Form
-                name="basic"
-                form={form}
-                initialValues={{ remember: true }}
-                onFinish={onFinish}
-                autoComplete="off"
-                requiredMark={false}
-                className="customAddForm"
-            >
-                <Form.List name="ownerDetails">
-                    {(fields, { add, remove }) => (
-                        <>
-                            {ownerInfoData.map((field, index) => (
-                                <div
-                                    style={{ marginTop: 2 }}
-                                    key={index}
-                                    className="row"
-                                >
-                                    {/* {form.type === "new" && (
+  return (
+    <div>
+      <Form
+        name="basic"
+        form={form}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        autoComplete="off"
+        requiredMark={false}
+        className="customAddForm"
+      >
+        <Form.List name="ownerDetails">
+          {(fields, { add, remove }) => (
+            <>
+              {ownerInfoData.map((field, index) => (
+                <div style={{ marginTop: 2 }} key={index} className="row">
+                  {/* {form.type === "new" && (
                     <hr className={styles.ownerInfoCardLine} />
                   )} */}
-                                    <OwnerInfoCardBox
-                                        index={index}
-                                        field={field}
-                                        remove={remove}
-                                        //  canDelete={form.type === "new"}
-                                        onDeleteCardClick={onDeleteCardClick}
-                                        clientType={clientType}
-                                    />
-                                </div>
-                            ))}
-                        </>
-                    )}
-                </Form.List>
-                <div className="row">
-                    <div className={styles.formFooterAction}>
-                        <CardBottomAction
-                            addCardClick={addMoreOwnerCard}
-                            onChange={onChange}
-                        />
-                    </div>
+                  <OwnerInfoCardBox
+                    index={index}
+                    field={field}
+                    remove={remove}
+                    //  canDelete={form.type === "new"}
+                    onDeleteCardClick={onDeleteCardClick}
+                    partnerType={partnerType}
+                  />
                 </div>
-            </Form>
+              ))}
+            </>
+          )}
+        </Form.List>
+        <div className="row">
+          <div className={styles.formFooterAction}>
+            <CardBottomAction
+              addCardClick={addMoreOwnerCard}
+              onChange={onChange}
+            />
+          </div>
         </div>
-    );
+      </Form>
+    </div>
+  );
 };
 
 export default OwnerInfo;
