29c0a1e372d21e7d5de7aa404ee0e989f016b607
Pinank Soni <pinank@miwebsolution.com>
2023-10-23 18:45:00 +0530

    sub profile Edit changes

--- src/components/SubscriptionCard/Index.tsx
+++ src/components/SubscriptionCard/Index.tsx
@@ -3,26 +3,28 @@ import styles from "./subscriptionCard.module.scss";
 import SubscriptionCardContent from "./SubscriptionCardContent";
 
 const SubscriptionCard = ({
-    column = 4,
-    onChangeActiveClick,
-    handleViewBtnClick,
-    ...rest
+  column = 4,
+  onChangeActiveClick,
+  handleViewBtnClick,
+  handleProfileClick,
+  ...rest
 }: any) => {
-    return (
-        <div
-            className={classNames(
-                styles.subscriptionCardWrapper,
-                "card card-body mt-2 shadow-none bg-100 mb-3"
-            )}
-        >
-            <SubscriptionCardContent
-                {...rest}
-                column={column}
-                onChangeActiveClick={onChangeActiveClick}
-                handleViewBtnClick={handleViewBtnClick}
-            />
-        </div>
-    );
+  return (
+    <div
+      className={classNames(
+        styles.subscriptionCardWrapper,
+        "card card-body mt-2 shadow-none bg-100 mb-3"
+      )}
+    >
+      <SubscriptionCardContent
+        {...rest}
+        column={column}
+        onChangeActiveClick={onChangeActiveClick}
+        handleViewBtnClick={handleViewBtnClick}
+        handleProfileClick={handleProfileClick}
+      />
+    </div>
+  );
 };
 
 export default SubscriptionCard;
