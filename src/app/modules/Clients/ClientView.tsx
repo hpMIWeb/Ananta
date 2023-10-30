e687e41cf13154f97fc8aa23476d7f2ff6540f7c
Pinank Soni <pinank@miwebsolution.com>
2023-10-30 18:36:57 +0530

    client view rest state issue solve

--- src/app/modules/Clients/ClientView.tsx
+++ src/app/modules/Clients/ClientView.tsx
@@ -14,134 +14,138 @@ import FormContentSkeletonLoader from "../../../components/FormContentSkeletonLo
 import { getClientsReducersApi } from "../../../redux/getClientsReducers";
 import { useAppDispatch } from "../../states/store";
 import AddClient from "./AddClient/Index";
+import { resetStateCreateClient } from "../../../redux/createAssociatePartnerReducers";
 
 const ClientView = () => {
-  const dispatch = useAppDispatch();
-  const navigation = useNavigate();
-  const { state } = useLocation();
-  const [form] = Form.useForm();
+    const dispatch = useAppDispatch();
+    const navigation = useNavigate();
+    const { state } = useLocation();
+    const [form] = Form.useForm();
 
-  const [activeTab, setActiveTab] = useState(1);
-  const [selectedClientData, setSelectedClientData] = useState<any>({});
+    const [activeTab, setActiveTab] = useState(1);
+    const [selectedClientData, setSelectedClientData] = useState<any>({});
 
-  const getClientsList = useSelector((state: any) => state.getClients.data);
-  const getClientsLoading = useSelector(
-    (state: any) => state.getClients.loading
-  );
-  const getClientListSuccess = useSelector(
-    (state: any) => state.getEmployees.success
-  );
+    const getClientsList = useSelector((state: any) => state.getClients.data);
+    const getClientsLoading = useSelector(
+        (state: any) => state.getClients.loading
+    );
+    const getClientListSuccess = useSelector(
+        (state: any) => state.getEmployees.success
+    );
 
-  const [clientId, setClientId] = useState<string>("");
+    const [clientId, setClientId] = useState<string>("");
 
-  const { loading, success } = useSelector(
-    (state: any) => state.createEmployee
-  );
+    const { loading, success } = useSelector(
+        (state: any) => state.createEmployee
+    );
 
-  useEffect(() => {
-    if (state) setClientId(state.id);
-  }, []);
-  useEffect(() => {
-    if (!getClientListSuccess) {
-      dispatch(getClientsReducersApi());
-    }
-  }, []);
+    useEffect(() => {
+        if (state) setClientId(state.id);
+    }, []);
+    useEffect(() => {
+        if (!getClientListSuccess) {
+            dispatch(getClientsReducersApi());
+        }
+    }, []);
 
-  const handleCancelClick = () => {
-    navigation("/caclient");
-  };
+    const handleCancelClick = () => {
+        navigation("/caclient");
+    };
 
-  useEffect(() => {
-    if (getClientsList.length && clientId) {
-      const currentCardDetail = getClientsList.find(
-        (s: any) => s._id === clientId
-      );
-      form.setFieldsValue(currentCardDetail);
-      setSelectedClientData(currentCardDetail);
-    }
-  }, [getClientsList, clientId, form]);
+    useEffect(() => {
+        if (getClientsList.length && clientId) {
+            const currentCardDetail = getClientsList.find(
+                (s: any) => s._id === clientId
+            );
+            form.setFieldsValue(currentCardDetail);
+            setSelectedClientData(currentCardDetail);
+        }
+    }, [getClientsList, clientId, form]);
 
-  useEffect(() => {
-    if (success) {
-      navigation("/caclient");
-    }
-  }, [success]);
+    useEffect(() => {
+        if (success) {
+            dispatch(resetStateCreateClient());
+            navigation("/caclient");
+        }
+    }, [success]);
 
-  const onChange = (key: any, formInfo: any) => {
-    setActiveTab(key);
-  };
+    const onChange = (key: any, formInfo: any) => {
+        setActiveTab(key);
+    };
 
-  const items = [
-    {
-      key: 1,
-      label: `Dashboard`,
-    },
-    {
-      key: 2,
-      label: `Profile`,
-      children: <AddClient selectedClientData={selectedClientData} />,
-    },
-    {
-      key: 6,
-      label: `Setting`,
-    },
-  ];
+    const items = [
+        {
+            key: 1,
+            label: `Dashboard`,
+        },
+        {
+            key: 2,
+            label: `Profile`,
+            children: <AddClient selectedClientData={selectedClientData} />,
+        },
+        {
+            key: 6,
+            label: `Setting`,
+        },
+    ];
 
-  return (
-    <div className={classNames("card mb-3", styles.addPromoCodeCardWrapper)}>
-      <div
-        className={classNames(
-          "card-header d-flex",
-          styles.promoCodeCardHeaderBox
-        )}
-        style={{ minHeight: 60 }}
-      >
+    return (
         <div
-          className={classNames(
-            "d-flex align-items-center w-100",
-            styles.promocodeHeaderTitle
-          )}
+            className={classNames("card mb-3", styles.addPromoCodeCardWrapper)}
         >
-          <div className="me-auto">
-            <h5
-              className={classNames(
-                "my-2 position-relative z-index-1",
-                styles.addPromoCodeLabel
-              )}
+            <div
+                className={classNames(
+                    "card-header d-flex",
+                    styles.promoCodeCardHeaderBox
+                )}
+                style={{ minHeight: 60 }}
             >
-              {selectedClientData.firmName}
-            </h5>
-          </div>
-          <div className={classNames("ms-auto z-index-1")}>
-            <Button
-              onClick={handleCancelClick}
-              className="greyBtn"
-              shape="round"
-            >
-              <Icon
-                height={14}
-                width={8.75}
-                name="prevArrow"
-                className="rotateRevere"
-              />
-            </Button>
-          </div>
+                <div
+                    className={classNames(
+                        "d-flex align-items-center w-100",
+                        styles.promocodeHeaderTitle
+                    )}
+                >
+                    <div className="me-auto">
+                        <h5
+                            className={classNames(
+                                "my-2 position-relative z-index-1",
+                                styles.addPromoCodeLabel
+                            )}
+                        >
+                            {selectedClientData.firmName}
+                        </h5>
+                    </div>
+                    <div className={classNames("ms-auto z-index-1")}>
+                        <Button
+                            onClick={handleCancelClick}
+                            className="greyBtn"
+                            shape="round"
+                        >
+                            <Icon
+                                height={14}
+                                width={8.75}
+                                name="prevArrow"
+                                className="rotateRevere"
+                            />
+                        </Button>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.addEmployeeDetailBox}>
+                {getClientsLoading && clientId && <FormContentSkeletonLoader />}
+                {!(getClientsLoading && clientId) && (
+                    <Tabs
+                        className="subscriptionTabs"
+                        defaultActiveKey="1"
+                        activeKey={activeTab}
+                        items={items}
+                        onChange={onChange}
+                    />
+                )}
+            </div>
         </div>
-      </div>
-      <div className={styles.addEmployeeDetailBox}>
-        {getClientsLoading && clientId && <FormContentSkeletonLoader />}
-        {!(getClientsLoading && clientId) && (
-          <Tabs
-            className="subscriptionTabs"
-            defaultActiveKey="1"
-            activeKey={activeTab}
-            items={items}
-            onChange={onChange}
-          />
-        )}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ClientView;
